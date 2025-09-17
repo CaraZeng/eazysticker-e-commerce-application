86fc4643ecb58af5b53be7d8ef37724926e74b8e
cara <carazengca@gmail.com>
2025-09-17 10:08:06 -0700

    debug: check env loading

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -1,10 +1,10 @@
-console.log("VITE_API_BASE_URL = ", import.meta.env.VITE_API_BASE_URL);
-
 import Header from "./components/Header";
 import Footer from "./components/footer/Footer";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { useNavigation } from "react-router-dom";
+
+console.log("VITE_API_BASE_URL = ", import.meta.env.VITE_API_BASE_URL);
 
 function App() {
   const navigation = useNavigation();
